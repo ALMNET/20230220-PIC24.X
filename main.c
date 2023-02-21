f8fee1b29df70ca3597033f21eed16605e76652a
Armando Almarales <armandoalmarales@eycon.com>
2023-02-21 16:58:34 -0300

    Comments Added

--- main.c
+++ main.c
@@ -91,77 +91,76 @@ int main(void)
     LED1_SetDigitalOutput();
     LED2_SetDigitalOutput();
     
-    
-
-    
-    
     do{
         switch(OPER_STATUS){
-        case RESET:
             
-            LED1_Toggle();
-            LED2_Toggle();
-            delay_ms(500);
+            ////////////////////////////////////////////////////////////////////
+            case RESET:
 
-            LED1_Toggle();
-            LED2_Toggle();
-            delay_ms(500);
-
-            LED1_Toggle();
-            LED2_Toggle();
-            delay_ms(500);
-
-            printf("\n\nOK");
-            
-            while(adc_value_get() < 50);
-            
-            OPER_STATUS = SENSOR_ALARM_MODE;
-
-            break;
-
-        case SENSOR_ALARM_MODE:
-            
-            
-            printf("\nSENSOR ALARM");
-            
-            do{
-                potValue = adc_value_get();
+                // First toggle
                 LED1_Toggle();
-                delay_ms(10 * (100 - potValue));
-            }while(PB1_GetValue());
-            
-            LED1_SetHigh();
-            OPER_STATUS = SENSOR_ALARM_ACK_MODE;
-            
-            break;
+                LED2_Toggle();
+                delay_ms(500);
 
-        case SENSOR_ALARM_ACK_MODE:
-            
-            printf("\nSENSOR ALARM ACK");
-            
-            while(PB2_GetValue());
-            LED2_SetLow();
-            
-            printf("\nSENSOR ALARM CLEARED");
-            delay_ms(2000);
-            
-            LED1_SetLow();
-            
-            OPER_STATUS = RESET;
-            
-            break;
+                // Second toggle
+                LED1_Toggle();
+                LED2_Toggle();
+                delay_ms(500);
 
+                // Third toggle (Last one)
+                LED1_Toggle();
+                LED2_Toggle();
+                delay_ms(500);
+
+                printf("\n\nOK");
+                delay_ms(2000); // 2 secs message
+
+                while(adc_value_get() < 50);    // Waits until pot > 50%
+
+                OPER_STATUS = SENSOR_ALARM_MODE;    // Change state to Sensor Alarm
+
+                break;
+
+
+            ////////////////////////////////////////////////////////////////////   
+            case SENSOR_ALARM_MODE:
+
+
+                printf("\nSENSOR ALARM");
+
+                do{
+                    potValue = adc_value_get();
+                    
+                    // LED1 Oscilation according pot value
+                    LED1_Toggle();
+                    delay_ms(10 * (100 - potValue));
+                }while(PB1_GetValue()); // Waits until PB1 is pressed
+
+                LED1_SetHigh();
+                OPER_STATUS = SENSOR_ALARM_ACK_MODE;
+
+                break;
+
+            ////////////////////////////////////////////////////////////////////
+            case SENSOR_ALARM_ACK_MODE:
+
+                printf("\nSENSOR ALARM ACK");
+
+                while(PB2_GetValue());
+                LED2_SetLow();
+
+                printf("\nSENSOR ALARM CLEARED");
+                delay_ms(2000);
+
+                LED1_SetLow();
+
+                OPER_STATUS = RESET;
+
+                break;
+
+            ////////////////////////////////////////////////////////////////////
         }           
     }while (1);
-    
-    do{
-        potValue = adc_value_get();
-        LATA =  potValue;
-        printf("\n%u", potValue);
-        LED1_Toggle();
-        delay_ms(500);
-        
-    }while(1);
         
     return 1;
 }
@@ -181,14 +180,30 @@ uint16_t adc_value_get(){
 
     ADC1_Enable();
     ADC1_ChannelSelect(Potentiometer);
-    ADC1_SoftwareTriggerEnable();
-    //Provide Delay
+    
+    // Conversion Enable
+    ADC1_SoftwareTriggerEnable();   
+    
+    // Time Adquisition delay (TAD) for ADC Conversion
     for(int i = 0; i < 1000; i++)
     {
     }
+    
+    // Stops ADC Trigger conversion
     ADC1_SoftwareTriggerDisable();
+    
+    // Waits for ADC to complete process 
     while(!ADC1_IsConversionComplete(Potentiometer));
+    
+    // Adjusting value to 0 - 100 result value -> 100 / 1023 = 0.0977517106
+    
+    // Result = ADC_Value * Maximum desired value / Maximum ADC Value)
+    // Where:
+    // 100 -> Maximum desired value, comes from 100%
+    // 1023 -> Maximum ADC Value
     result = (uint16_t) (ADC1_ConversionResultGet(Potentiometer) * 0.0977517106);
+    
+    // ADC Module disabled for decrease power consumption 
     ADC1_Disable(); 
     
     return result;
